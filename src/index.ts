ef0d339d54d4510ae64b3d662881149bdef89742
Carlos Mazzarri <cjmazzarri@gmail.com>
2023-09-24 18:07:11 -0500

    🗃️Added DB connection

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+require('dotenv').config();
 
 const app = express();
 app.use(cors({
@@ -19,3 +21,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/')
 })
+
+const MONGO_URL = 'mongodb+srv://'+process.env.MONGO_USER+':'+process.env.MONGO_PWD+'@nodejs-api-tutorial.esno4jg.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error))
