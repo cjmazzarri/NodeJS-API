cb3b561ceb3c3af3f2bf85dc68c0e86ef6a3376d
Carlos Mazzarri <cjmazzarri@gmail.com>
2023-09-24 18:45:51 -0500

    🛂Added auth route

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
 require('dotenv').config();
 
 const app = express();
@@ -28,3 +29,4 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error))
 
+app.use('/', router());
